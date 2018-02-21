7fe1f6774d25293e1a98b8e3cd51ce0bd4b9d57f
kruemel <weihek@gmail.com>
2018-02-21 12:52:24 +0100

    navio2 ws

---
+++ Navio2/Python/motor.py
@@ -0,0 +1,28 @@
+import sys
+import time
+
+import navio.pwm
+import navio.util
+
+navio.util.check_apm()
+
+PWM_OUTPUT = 1
+SERVO_MIN = 1.250 #ms
+SERVO_MAX = 1.750 #ms
+
+with navio.pwm.PWM(PWM_OUTPUT) as pwm:
+    pwm.set_period(50)
+    pwm.enable()
+
+#    while (True):
+#        pwm.set_duty_cycle(SERVO_MIN)
+#        time.sleep(1)
+#        pwm.set_duty_cycle(SERVO_MAX)
+#        time.sleep(1)
+
+    pwm.set_duty_cycle(1.600)
+    print("Motor started")
+    time.sleep(10)
+    pwm.set_duty_cycle(1.500)
+    pwm.disable()
+
