eb5867a6120e5a2c7d39af68dc8bbba1e306f879
albertkasdorf <albert.kasdorf@live.de>
2018-02-22 08:33:14 +0100

    Bla 5

--- scripts/set_rights.sh
+++ scripts/set_rights.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm0/enable
+sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm0/period
+sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm1/enable
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm1/period
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm1/duty_cycle
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm2/enable
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm2/period
 sudo chmod a+rw /sys/class/pwm/pwmchip0/pwm2/duty_cycle
+sudo chmod a+rw /sys/class/pwm/pwmchip0/export
+sudo chmod a+rw /sys/class/pwm/pwmchip0/unexport
+
+roslaunch bcx18_drift motor.launch
